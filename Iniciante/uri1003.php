9b6d954ba98848f494eaf70767da9d499452d47f
Pedro Henrique <pedropires2603@gmail.com>
2021-04-16 16:50:59 -0300

    Removendo arquivos desorganizados

--- Iniciante/uri1003.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $A = fgets(STDIN);
-    $B = fgets(STDIN);
-    $SOMA = $A + $B;
-    echo "SOMA = $SOMA\n";
-?>
--- Iniciante/uri1005.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $nota1 = fgets(STDIN) * 3.5;
-    $nota2 = fgets(STDIN) * 7.5;
-    $MEDIA = number_format(($nota1+$nota2)/11,5,'.','');
-    echo "MEDIA = $MEDIA\n";
-?>
